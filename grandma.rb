303d281c70e98553da4b86a69c55bb41681c27b2
Paige Skinner <skinnerpa86@yahoo.com>
2017-12-01 16:49:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
-    if phrase.upcase() && != "I LOVE YOU GRANDMA!"
+    if phrase.upcase() && phrase != "I LOVE YOU GRANDMA!"
       return "NO, NOT SINCE 1938!"
 
     elsif
